f3c04b4457240a0a37eb8376706d8f5a3f1a75fe
Genexi <Pashaz1994@gmail.com>
2023-06-29 14:55:01 +0300

    one

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
